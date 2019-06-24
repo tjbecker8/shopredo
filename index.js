82a375560718541486a3d904185c5d0a03112aab
Tom Becker <tjbecker@me.com>
2019-06-24 11:00:24 +0700

    added env package and port

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 
 const dp = require('./db')
 
+require('dotenv').config()
+
 app.use(express.static(path.join(__dirname, 'client')))
 
 app.get('/', (req, res) => {
@@ -15,6 +17,6 @@ app.get('/api/products', require('./controllers/products_get.js'))
 app.get('/api/categories', require('./controllers/categories_get.js'))
 app.get('/api/click/:id', require('./controllers/click_get.js'))
 
-app.listen(3000, () => {
-	console.log('Server listening on port 3000');
+app.listen(process.env.PORT, () => {
+	console.log(`Server listening on port ${process.env.PORT}`);
 })
